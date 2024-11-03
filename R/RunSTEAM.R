2fe556012ef2ed5897e63a12eb56f09121de7bf7
Sam Reynoso <90324837+SNOSO@users.noreply.github.com>
2024-11-02 21:10:06 -0600

    Update RunSTEAM.R

--- R/RunSTEAM.R
+++ R/RunSTEAM.R
@@ -5,8 +5,8 @@ RunSTEAM <- function(matrix, coordinates, labels, train_ratio, n_size, seed, n_t
   test_matrix <- data$test$matrix
   train_labels <- data$train$labels
   test_labels <- data$test$labels
-  train_coords <- data$train$coords
-  test_coords <- data$test$coords
+  train_coords <- data$train$coordinates
+  test_coords <- data$test$coordinates
   
   avg_train_matrix <- neighborhood_averaging(train_matrix, train_coords, train_labels, n_size, seed)
   avg_test_matrix <- neighborhood_averaging(test_matrix, test_coords, test_labels, n_size, seed)
