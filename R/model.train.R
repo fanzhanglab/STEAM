dff2f8c7875a2fe208f53c52473345cc2ced9a59
Revanth Krishna <revukrishna2000@gmail.com>
2024-11-19 18:38:22 -0700

    Model train update

--- R/model.train.R
+++ R/model.train.R
@@ -1,8 +1,11 @@
 library(randomForest)
 library(e1071)
 library(caret)
+library(doParallel)
+cl <- makePSOCKcluster(5)
+registerDoParallel(cl)
 
-model.train <- function(matrix, labels, model, seed, n_tree = NULL, kernel = NULL, scale = TRUE, cv_folds = 5, folder_name = 'train.out') {
+model.train <- function(matrix, labels, model, seed, n_tree = NULL, kernel = NULL, scale = TRUE, cv_folds = 10, folder_name = 'train.out') {
   set.seed(seed)
 
   # Prepare the data frame
@@ -16,61 +19,36 @@ model.train <- function(matrix, labels, model, seed, n_tree = NULL, kernel = NUL
   train_data <- df[train_index, ]
   valid_data <- df[-train_index, ]
 
-  train_control <- trainControl(method = "cv", number = cv_folds)
+  train_control <- trainControl(method = "cv", number = cv_folds, savePredictions = "all", classProbs = TRUE, p = 0.8, search = 'grid', allowParallel = TRUE)
 
   if (model == "rf") {
     # Random Forest model with cross-validation
-    train_model <- train(labels ~ ., data = train_data, method = "rf", trControl = train_control, ntree = n_tree)
+    tune_grid <- expand.grid(mtry = var_seq(p = sqrt(ncol(train_data)), classification = T, len=10))
+    train_model <- train(labels ~ ., data = train_data, method = "rf", trControl = train_control, ntree = 200, tunegrid = tune_grid, metric = 'Kappa')
     message("Finished RandomForest model building with cross-validation")
 
   } else if (model == "svm") {
     # SVM model with cross-validation
-    train_model <- train(labels ~ ., data = train_data, method = "svmRadial", trControl = train_control,
-                       preProcess = c("center", "scale"))
+    train_model <- train(labels ~ ., data = train_data, method = "svmRadial", trControl = train_control, metric = 'Kappa')
     message("Finished SVM model building with cross-validation")
 
 
   } else {
     stop("Model type is not supported. Please choose 'rf' or 'svm'.")
   }
-  train_pred <- predict(train_model, train_data, type = "prob")
-  valid_pred <- predict(train_model, valid_data, type = "prob")
-  train_roc <- multiclass.roc(train_data$labels, train_pred)
-  valid_roc <- multiclass.roc(valid_data$labels, valid_pred)
+
+  stopCluster(cl)
 
   # Create output folder if it doesn't exist
   if (!dir.exists(folder_name)) {
     dir.create(folder_name)
   }
 
-  # # Generate ROC Plot for Training and Validation
-  # plot_roc <- function(roc_obj, title) {
-  #   aucs <- sapply(roc_obj$rocs, function(r) auc(r))
-  #   avg_auc <- mean(aucs)
-  #   roc_plot <- ggplot() +
-  #     labs(title = paste0(title, "\nMean AUC: ", round(avg_auc, 3))) +
-  #     theme_minimal()
-  #   for (r in roc_obj$rocs) {
-  #     roc_df <- data.frame(Sensitivity = r$sensitivities, Specificity = 1 - r$specificities)
-  #     roc_plot <- roc_plot + geom_line(data = roc_df, aes(x = Specificity, y = Sensitivity), alpha = 0.5)
-  #   }
-  #   return(roc_plot)
-  # }
-  #
-  # # Save Plots to PDF
-  # pdf(file.path(folder_name, "train_roc_plots.pdf"), width = 10, height = 5)
-  # train_roc_plot <- plot_roc(train_roc, "Training ROC Curves")
-  # valid_roc_plot <- plot_roc(valid_roc, "Validation ROC Curves")
-  # grid.arrange(train_roc_plot, valid_roc_plot, ncol = 2)
-  # dev.off()
 
-
-
-
-  # Save Model and Metrics
   train_acc <- mean(predict(train_model, train_data) == train_data$labels)
+  print(paste("Validation Accuracy:", train_acc))
+
   valid_acc <- mean(predict(train_model, valid_data) == valid_data$labels)
-  print(paste("Training Accuracy:", train_acc))
   print(paste("Validation Accuracy:", valid_acc))
 
   saveRDS(train_model, file = file.path(folder_name, "train.model.rds"))
