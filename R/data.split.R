8d62eca109bacf55d9c6bcbca1a468bd4b66a88e
Sam Reynoso <90324837+SNOSO@users.noreply.github.com>
2025-05-21 15:37:03 -0600

    Update data.split.R

--- R/data.split.R
+++ R/data.split.R
@@ -1,15 +1,31 @@
-data.split <- function(STEAM.obj, train.ratio){
-
+data.split <- function(STEAM.obj, train.ratio) {
+  
+  # Universal labels
+  clean_labels <- function(labels) {
+    labels <- as.character(labels)
+    labels <- gsub("/", ".", labels)
+    labels <- gsub("-", ".", labels)
+    labels <- gsub(" ", "_", labels)
+    labels <- gsub("[()]", "", labels)
+    labels <- gsub(",", "", labels)
+    labels <- gsub("\\.\\.", ".", labels)
+    labels <- trimws(labels)
+    labels <- iconv(labels, to = "ASCII//TRANSLIT")
+    return(labels)
+  }
+  
+  STEAM.obj$labels <- clean_labels(STEAM.obj$labels)
+  
+  # Create train/test split
   ind <- createDataPartition(STEAM.obj$labels, p = train.ratio, list = FALSE)
-
+  
   STEAM.obj$train$train.data.matrix <- STEAM.obj$count_exp[, ind]
   STEAM.obj$train$train.data.coords <- STEAM.obj$spatial[ind, ]
   STEAM.obj$train$train.data.labels <- STEAM.obj$labels[ind]
-
+  
   STEAM.obj$test$test.data.matrix <- STEAM.obj$count_exp[, -ind]
-  STEAM.obj$test$test.data.coords <- STEAM.obj$spatial[-ind,]
+  STEAM.obj$test$test.data.coords <- STEAM.obj$spatial[-ind, ]
   STEAM.obj$test$test.data.labels <- STEAM.obj$labels[-ind]
-
+  
   return(STEAM.obj)
-
 }
